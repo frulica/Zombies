37359f30864fb271e3a761f0d444f34981f44eee
GAMECHUCKBEAST\Jurica <juricacv@gmail.com>
2023-06-14 15:55:39 +0200

    Diddling zombies, spawning tombstones

--- Assets/Scripts/ComponentsAndTags/GraveyardAspect.cs
+++ Assets/Scripts/ComponentsAndTags/GraveyardAspect.cs
@@ -1,5 +1,9 @@
 ﻿using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UIElements;
 
 namespace TMG.Zombies
 {
@@ -8,9 +12,43 @@ namespace TMG.Zombies
         public readonly Entity Entity;
 
         private readonly RefRO<LocalTransform> _transform;
+        private LocalTransform Transform => _transform.ValueRO;
 
         private readonly RefRO<GraveyardProperties> _graveyardProperties;
         private readonly RefRW<GraveyardRandom> _graveyardRandom;
 
+        public int NumberOfTombstonesToSpawn => _graveyardProperties.ValueRO.NumberOfTombstonesToSpawn;
+        public Entity TombstonePrefab => _graveyardProperties.ValueRO.TombstonePrefab;
+
+        public LocalTransform GetRandomTombstoneTransform()
+        {
+            return new LocalTransform
+            {
+                Position = GetRandomPosition(),
+                Rotation = Quaternion.identity,
+                Scale = 1
+            };
+        }
+
+        private float3 GetRandomPosition()
+        {
+            float3 randomPosition;
+            do
+            {
+                randomPosition = _graveyardRandom.ValueRW.Value.NextFloat3(MinCorner, MaxCorner);
+            } while (math.distancesq(Transform.Position, randomPosition) <= BRAIN_SAFETY_RADIUS_SQ);
+
+            return randomPosition;
+        }
+
+        private float3 MinCorner => Transform.Position - HalfDimensions;
+        private float3 MaxCorner => Transform.Position + HalfDimensions;
+        private float3 HalfDimensions => new()
+        {
+            x = _graveyardProperties.ValueRO.FieldDimensions.x * 0.5f,
+            y = 0f,
+            z = _graveyardProperties.ValueRO.FieldDimensions.y * 0.5f
+        };
+        private const float BRAIN_SAFETY_RADIUS_SQ = 100;
     }
 }
---
+++ Assets/Scripts/Systems/SpawnTombstoneSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq.Expressions;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace TMG.Zombies
+{
+    [BurstCompile]
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    public partial struct SpawnTombstoneSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<GraveyardProperties>();
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            state.Enabled = false;
+            var graveyardEntity = SystemAPI.GetSingletonEntity<GraveyardProperties>();
+            var graveyard = SystemAPI.GetAspect<GraveyardAspect>(graveyardEntity);
+
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+            for (var i = 0; i < graveyard.NumberOfTombstonesToSpawn; i++)
+            {
+                var newTombstone = ecb.Instantiate(graveyard.TombstonePrefab);
+                var newTombstoneTransform = graveyard.GetRandomTombstoneTransform();
+                ecb.SetComponent(newTombstone,newTombstoneTransform);
+            }
+
+            ecb.Playback(state.EntityManager);
+        }
+    }
+}
